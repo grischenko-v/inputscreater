c42348d7fc692fe6aad3731e475804ed0463efb1
Vladimir Grishchenko <vgrishchenko@zoon.ru>
2020-10-07 21:28:39 +0300

    Updates

---
+++ precache-manifest.57481fa3df0c7fa4f3b90c1deae20a55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d53c10259b83e0dfd30d68faa1d81a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "459853239db3dad264c5",
+    "url": "/static/css/2.220ccb42.chunk.css"
+  },
+  {
+    "revision": "0b67ee7b23eb1eb0226c",
+    "url": "/static/css/main.63237c89.chunk.css"
+  },
+  {
+    "revision": "459853239db3dad264c5",
+    "url": "/static/js/2.fbd07c63.chunk.js"
+  },
+  {
+    "revision": "e4f6fd5e687738d725e56603861f1923",
+    "url": "/static/js/2.fbd07c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b67ee7b23eb1eb0226c",
+    "url": "/static/js/main.0181eb98.chunk.js"
+  },
+  {
+    "revision": "c4f030ff2a26f0cd0cbc",
+    "url": "/static/js/runtime-main.ea7a341e.js"
+  }
+]);
